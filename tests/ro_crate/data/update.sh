fd4ca6e559c8a5fdf41c8432aa35da1e5e2489e2
lrodrin <lrodrin@gmail.com>
2020-10-27 09:27:04 +0100

    deleted old code

--- tests/ro_crate/data/update.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# Update version number from https://schema.org/docs/releases.html
-curl -L -f -o schema.jsonld http://schema.org/version/5.0/schema.jsonld
-# Apache License 2.0 https://github.com/schemaorg/schemaorg/blob/v5.0-release/LICENSE

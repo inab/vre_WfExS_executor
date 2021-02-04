a027d427650dff99de8962e58b7020c7e12db319
Laura Rodríguez Navas <lrodrin@users.noreply.github.com>
2021-02-04 17:11:26 +0100

    create model.py

---
+++ lib/model.py
@@ -0,0 +1,102 @@
+from dataclasses import dataclass
+from typing import List, Optional, Any
+
+
+@dataclass
+class Argument:
+    name: str
+    description: str
+    help: str
+    type: str
+    default: int
+
+
+@dataclass
+class Cloud:
+    launcher: str
+    workflow_type: str
+    minimum_v_ms: int
+    initial_v_ms: int
+    default_cloud: bool
+
+
+@dataclass
+class Clouds:
+    mug_irb: Cloud
+
+
+@dataclass
+class Infrastructure:
+    memory: int
+    cpus: int
+    executable: str
+    clouds: Clouds
+
+
+@dataclass
+class InputFile:
+    name: str
+    description: str
+    help: str
+    file_type: List[str]
+    data_type: List[str]
+    required: bool
+    allow_multiple: bool
+
+
+@dataclass
+class InputFilesCombination:
+    description: str
+    input_files: List[str]
+
+
+@dataclass
+class MetaData:
+    visible: bool
+    description: str
+
+
+@dataclass
+class File:
+    file_type: str
+    data_type: str
+    meta_data: MetaData
+    file_path: Optional[str] = None
+    compressed: Optional[str] = None
+
+
+@dataclass
+class OutputFile:
+    name: str
+    required: bool
+    allow_multiple: bool
+    file: File
+
+
+@dataclass
+class Owner:
+    author: str
+    institution: str
+    contact: str
+    url: str
+
+
+@dataclass
+class Tool:
+    id: str
+    name: str
+    title: str
+    short_description: str
+    long_description: str
+    url: str
+    publication: str
+    owner: Owner
+    keywords: List[str]
+    keywords_tool: List[str]
+    infrastructure: Infrastructure
+    input_files: List[InputFile]
+    input_files_public_dir: List[Any]
+    input_files_combinations: List[InputFilesCombination]
+    arguments: List[Argument]
+    has_custom_viewer: bool
+    output_files: List[OutputFile]

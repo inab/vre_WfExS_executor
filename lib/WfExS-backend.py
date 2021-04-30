b09e61a72e4e49e52128773d2230b71b76e8b450
lrodrin <laura.rodriguez@bsc.es>
2021-04-30 14:53:36 +0200

    created minimal main to fetch, setup engine and materialize containers using WfExS library

--- lib/WfExS-backend.py
+++ lib/WfExS-backend.py
@@ -14,37 +14,73 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import argparse
+import logging
+import sys
 
 import yaml
 import os
 
-from wfexs_backend.workflow import WF
 from yaml import Loader as YAMLLoader
 
-local_config = {}
-with open("tests/local_config.yaml", mode="r", encoding="utf-8") as cf:
-    local_config = yaml.load(cf, Loader=YAMLLoader)
+from wfexs_backend.workflow import WF
 
-workflow_meta = {}
-with open("tests/cosifer_test1_cwl.yaml", mode="r", encoding="utf-8") as wcf:
-    workflow_meta = yaml.load(wcf, Loader=YAMLLoader)
+if __name__ == '__main__':
 
-wfInstance = WF(local_config, "tests")
-wfInstance.fromForm(workflow_meta)
-print(wfInstance)
-wfInstance.fetchWorkflow()
-localWF = os.path.join(wfInstance.localWorkflow.dir, wfInstance.localWorkflow.relPath)
-print(localWF)
-wfInstance.setupEngine()
-wfInstance.materializeWorkflow()
-# with open(localWF, "r") as wf:
-#     wf = yaml.safe_load(wf)
-#     print(wf)
-#     inputs = wf['inputs']
-#     outputs = wf['outputs']
-#     print(inputs)
-# print(outputs)
+    # Basic main for minimal execution
+    ap = argparse.ArgumentParser(description="WfExS (workflow execution service) backend")
+    ap.add_argument('-d', '--debug', dest='logLevel', action='store_const', const=logging.DEBUG,
+                    help='Show debug messages (use with care, as it can disclose passphrases and passwords)')
+    ap.add_argument('-L', '--local-config', dest="localConfigFilename", help="Local installation configuration file")
+    ap.add_argument('-W', '--workflow-config', dest="workflowConfigFilename",
+                    help="Configuration file, describing workflow and inputs")
+    ap.add_argument('command', help='Command to run', nargs='?', default='execute')
 
-# fetchWorkflow()
-# materializeInputs()
-# CONTAINERS
+    args = ap.parse_args()
+
+    # Setting up the log
+    loggingConf = {
+        'format': '%(asctime)-15s - [%(levelname)s] %(message)s',
+    }
+
+    if args.logLevel:
+        loggingConf['level'] = args.logLevel
+
+    logging.basicConfig(**loggingConf)
+
+    # First, try loading the configuration file
+    localConfigFilename = args.localConfigFilename
+    if localConfigFilename and os.path.exists(localConfigFilename):
+        with open(localConfigFilename, mode="r", encoding="utf-8") as cf:
+            local_config = yaml.load(cf, Loader=YAMLLoader)
+    else:
+        local_config = {}
+        if localConfigFilename and not os.path.exists(localConfigFilename):
+            print("[WARNING] Configuration file {} does not exist".format(localConfigFilename), file=sys.stderr)
+
+    # Second, try loading the workflow configuration file
+    workflowConfigFilename = args.workflowConfigFilename
+    if workflowConfigFilename and os.path.exists(workflowConfigFilename):
+        with open(workflowConfigFilename, mode="r", encoding="utf-8") as wcf:
+            workflow_meta = yaml.load(wcf, Loader=YAMLLoader)
+
+    else:
+        workflow_meta = {}
+        if workflowConfigFilename and not os.path.exists(workflowConfigFilename):
+            print("[WARNING] Workflow configuration file {} does not exist".format(workflowConfigFilename),
+                  file=sys.stderr)
+
+    wfInstance = WF(local_config, "tests")  # TODO hardcoded path for the Crypt4GH keys
+    wfInstance.fromForm(workflow_meta)
+    if args.command == 'execute':
+        wfInstance.fetchWorkflow()
+        wfInstance.setupEngine()
+        wfInstance.materializeWorkflow()
+        localWF = os.path.join(wfInstance.localWorkflow.dir, wfInstance.localWorkflow.relPath)
+        localWFFile = open(localWF, "r")
+        localWFYAML = yaml.safe_load(localWFFile)
+
+        localWFInputs = localWFYAML['inputs']
+        localWFOutputs = localWFYAML['outputs']
+        print(localWFInputs)
+        print(localWFOutputs)
